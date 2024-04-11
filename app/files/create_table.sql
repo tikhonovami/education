4fea84dc68176b2b75c8134694f518f2f0b5395c
tikhonovami <148941798+tikhonovami@users.noreply.github.com>
2024-04-11 15:14:21 +0400

    final 1

--- app/files/create_table.sql
+++ app/files/create_table.sql
@@ -1,17 +1,17 @@
-    DROP TABLE IF EXISTS emp;
-    DROP TABLE IF EXISTS dept;
+DROP TABLE IF EXISTS emp;
+DROP TABLE IF EXISTS dept;
 
-    CREATE TABLE dept (
-        dept_id INTEGER PRIMARY KEY,
-        dname VARCHAR(20)
-        );
+CREATE TABLE dept (
+	dept_id INTEGER PRIMARY KEY,
+	dname VARCHAR(20)
+	);
 
-    CREATE TABLE emp (
-            emp_id INTEGER PRIMARY KEY,
-        dept_id INTEGER,
-        ename VARCHAR(15),
-        salary NUMERIC(6,2)
-    );
+CREATE TABLE emp (
+        emp_id INTEGER PRIMARY KEY,
+	dept_id INTEGER,
+	ename VARCHAR(15),
+	salary NUMERIC(6,2)
+);
 
-    ALTER TABLE emp
-    ADD CONSTRAINT dept_pk_emp FOREIGN KEY (dept_id) REFERENCES dept;
+ALTER TABLE emp
+ADD CONSTRAINT dept_pk_emp FOREIGN KEY (dept_id) REFERENCES dept;
